2d3a954348b1f3c9c7a3fea7255265ecbc5dac32
Hyeonkyo <zkdlajt96@gmail.com>
2021-10-14 19:34:38 +0900

    Debug : fix preprocessing

--- srcs/parse/parse.c
+++ srcs/parse/parse.c
@@ -135,22 +135,27 @@ int	find_separator(char *line, int idx)
 	return (FALSE);
 }
 
-char	**divide_by_separator(char *line, int sep_idx)
+char	**divide_by_separator(char *line, int start_idx, int sep_idx)
 {
 	char	**str;
 	int		front_len;
+	int		mid_len;
 	int		back_len;
 
-	str = (char **)malloc(sizeof(char *) * 2);
+	str = (char **)malloc(sizeof(char *) * 3);
 	merror(str);
-	front_len = sep_idx + 1;
+	front_len = start_idx + 1;
+	mid_len = sep_idx - start_idx + 1;
 	back_len = ft_strlen(line) - sep_idx + 1;
-	str[0] = (char *)malloc(sizeof(char) * front_len);
-	merror(str[0]);
-	str[1] = (char *)malloc(sizeof(char) * back_len);
-	merror(str[1]);
-	ft_strlcpy(str[0], line, front_len);
-	ft_strlcpy(str[1], line + sep_idx, back_len);
+	str[FRONT] = (char *)malloc(sizeof(char) * front_len);
+	merror(str[FRONT]);
+	str[MID] = (char *)malloc(sizeof(char) * mid_len);
+	merror(str[MID]);
+	str[BACK] = (char *)malloc(sizeof(char) * back_len);
+	merror(str[BACK]);
+	ft_strlcpy(str[FRONT], line, front_len);
+	ft_strlcpy(str[MID], line + start_idx, mid_len);
+	ft_strlcpy(str[BACK], line + sep_idx, back_len);
 	free(line);
 	return (str);
 }
@@ -159,7 +164,7 @@ char	*make_buf(void)
 {
 	char	*buf;
 
-	buf = (char *)malloc(sizeof(char) * 20000);//나중에 디파인 상수로 변경
+	buf = (char *)malloc(sizeof(char) * BUF_SIZE);//나중에 디파인 상수로 변경
 	merror(buf);
 	return (buf);
 }
@@ -207,7 +212,7 @@ char	*make_new_string(char *buf, int buf_len)
 {
 	char	*str;
 
-	str = (char *)malloc(sizeof(char) * (buf_len + 3));
+	str = (char *)malloc(sizeof(char) * (buf_len + 3));//양쪽에 ""넣어줄 공간까지 추가
 	merror(str);
 	str[0] = '\"';
 	ft_strlcpy(str + 1, buf, buf_len + 1);
@@ -217,15 +222,14 @@ char	*make_new_string(char *buf, int buf_len)
 	return (str);
 }
 
-char	*fillin_buf(char *buf, char *origin, int start_idx, t_info *info)
+char	*fillin_buf(char *buf, char *origin, t_info *info)
 {
 	int		i;
 	int		j;
 	t_type	type;
 
-	i = start_idx;
-	j = i;
-	ft_strlcpy(buf, origin, start_idx + 1);
+	i = 0;
+	j = 0;
 	while (origin[i])
 	{
 		type = check_type(origin[i]);
@@ -257,35 +261,42 @@ char	*fillin_buf(char *buf, char *origin, int start_idx, t_info *info)
 	return (make_new_string(buf, j));
 }
 
-char	*make_arrange_string(char *front, char *back, int *start_idx)
+char	*make_arrange_string(char *mid, char **divide, int *start_idx)
 {
+	char	*tmp;
 	char	*new;
 
-	*start_idx = ft_strlen(front);
-	new = ft_strjoin(front, back);
+	tmp = ft_strjoin(divide[FRONT], mid);
+	merror(tmp);
+	free(mid);
+	*start_idx = (int)ft_strlen(tmp);
+	new = ft_strjoin(tmp, divide[BACK]);
 	merror(new);
+	free(tmp);
 	return (new);
 }
 
 char	*clear_quote(char *line, int *start_idx, int sep_idx, t_info *info)
 {
 	/*
-	1. space인덱스를 구분자로 2개의 문자열로 쪼개기
-	2. int_max만큼 버퍼 할당.
-	3. 버퍼 채우기
+	1. start, sep인덱스를 구분자로 3개의 문자열로 쪼개기
+	2. 넉넉한 버퍼 할당. -> 기준 세우기
+	3. 가운데 문자열을 환경 변수 처리, quote처리 등 해서 버퍼 채우기
 	4. 채워진 버퍼 + 스페이스 + \0만큼 재할당후 문자열 만들어주고 버퍼 프리
-	5. 재할당 문자열과 기존 문자열 join후 start인덱스는 스페이스 다음으로 변경
+	5. 재할당 문자열과 기존 문자열 join
+	6. start인덱스를 구분자의 인덱스 위치로 변경해줌.
 	*/
 	char	*buf;
 	char	*new;
 	char	**divide;
 
-	//sep_idx >= start_idx 
-	divide = divide_by_separator(line, sep_idx);
+	if (sep_idx == 0)
+		sep_idx = ft_strlen(line);
+	divide = divide_by_separator(line, *start_idx, sep_idx);
 	buf = make_buf();
-	new = fillin_buf(buf, divide[0], *start_idx, info);
-	new = make_arrange_string(new, divide[1], start_idx);
-	free_env_list(divide);
+	new = fillin_buf(buf, divide[MID], info);
+	new = make_arrange_string(new, divide, start_idx);
+	free_double_string(divide);
 	return (new);
 }
 
@@ -376,6 +387,8 @@ char	*pre_processing(char *line, t_info *info)
 		if (start_idx && sep_idx == 0)
 			end_flag = TRUE;
 		new = clear_quote(new, &start_idx, sep_idx, info);
+		if (new == NULL)
+			return (NULL);
 		new = skip_space(new, &start_idx);
 		if (end_flag)
 			break ;
@@ -503,6 +516,7 @@ int	parse_line(char *line, t_info *info)
 	if (check_incorrect_line(line))
 		return (ERROR);
 	cmd = pre_processing(line, info);
+	//" '와 스페이스 처리함.
 	make_command(cmd, info);
 	return (NORMAL);
 }
