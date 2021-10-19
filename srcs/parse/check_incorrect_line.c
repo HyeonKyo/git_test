267f2aba4013b3adf9a18e513b5431cf22760d26
Hyeonkyo <zkdlajt96@naver.com>
2021-10-19 22:37:08 +0900

    Feat : add logic that check incorrect string.

--- srcs/parse/check_incorrect_line.c
+++ srcs/parse/check_incorrect_line.c
@@ -1,11 +1,5 @@
 #include "minishell.h"
 
-/*
-** =============================================================================
-** check_line.c
-** =============================================================================
-*/
-
 void	init_quote_data(t_quote *data)
 {
 	data->squote_flag = 0;
@@ -14,14 +8,39 @@ void	init_quote_data(t_quote *data)
 	data->dquote_cnt = 0;
 }
 
+static int	check_redirection(char *line, int i)
+{
+	int		redi_cnt;
+	t_type	type;
+
+	redi_cnt = 1;
+	type = is_redirection(line[i]);
+	while (type == is_redirection(line[i + redi_cnt]) && line[i + redi_cnt])
+		redi_cnt++;
+	if (redi_cnt <= 2 && !is_redirection(line[i + redi_cnt]))
+		return (TRUE);
+	return (FALSE);
+}
+
+static int	check_incorrect_case(char *line, int i)
+{
+	if ((line[i] == ';' || line[i] == '\\'))
+		return (TRUE);
+	if (check_type(line[i]) == PIPE && check_type(line[i + 1] == PIPE))
+		return (TRUE);
+	if (is_redirection(line[i]) && check_redirection(line, i))
+		return (TRUE);
+	return (FALSE);
+}
+
 int	check_incorrect_line(char *line)
 {
 	int		i;
 	t_quote	data;
 
 	init_quote_data(&data);
-	i = 0;
-	while (line[i])
+	i = -1;
+	while (line[++i])
 	{
 		if (check_type(line[i]) == SQUOTE && data.dquote_flag == FALSE)
 		{
@@ -33,15 +52,9 @@ int	check_incorrect_line(char *line)
 			data.dquote_cnt++;
 			data.dquote_flag ^= TRUE;
 		}
-		if (!data.squote_flag && !data.dquote_flag &&
-			(line[i] == ';' || line[i] == '\\'))
-			return (TRUE);
-		//리다이렉션 체크 추가
-		//1. <>처럼 다른 게 연속으로 올 떄
-		//2. >>> 처럼 3개 이상이 연속으로 올때
-		//double pipe인지
-		//맨처음line[0]이 separator인지
-		i++;
+		if (!data.squote_flag && !data.dquote_flag)
+			if (check_incorrect_case(line, i))
+				return (TRUE);
 	}
 	if ((data.squote_cnt & ISODD) || (data.dquote_cnt & ISODD))
 		return (TRUE);
